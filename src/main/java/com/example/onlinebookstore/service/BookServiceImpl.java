2e7f61279fe89323ef203e4999168f6c5cfd60f5
bodya4243 <105318502+bodya4243@users.noreply.github.com>
2024-04-21 21:35:17 +0300

    add Book entity add BookRepository add BookService configure database access configure checkstyle

---
+++ src/main/java/com/example/onlinebookstore/service/BookServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.onlinebookstore.service;
+
+import com.example.onlinebookstore.model.Book;
+import com.example.onlinebookstore.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List findAll() {
+        return bookRepository.findAll();
+    }
+}
