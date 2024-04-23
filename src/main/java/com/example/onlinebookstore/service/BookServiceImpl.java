1a82679358c1e30bd3db03e7fe620d28e31af7a4
bodya4243 <105318502+bodya4243@users.noreply.github.com>
2024-04-23 15:17:18 +0300

    1) Hide credentials 2) Remove Data annotation and add @Getter @Setter

--- src/main/java/com/example/onlinebookstore/service/BookServiceImpl.java
+++ src/main/java/com/example/onlinebookstore/service/BookServiceImpl.java
@@ -3,17 +3,13 @@ package com.example.onlinebookstore.service;
 import com.example.onlinebookstore.model.Book;
 import com.example.onlinebookstore.repository.BookRepository;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
-
-    @Autowired
-    public BookServiceImpl(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
 
     @Override
     public Book save(Book book) {
@@ -21,7 +17,7 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public List findAll() {
+    public List<Book> findAll() {
         return bookRepository.findAll();
     }
 }
