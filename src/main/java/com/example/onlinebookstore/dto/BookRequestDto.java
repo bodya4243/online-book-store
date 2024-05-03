b66b5671aa0b10e654efd908a578335765f1553e
bodya4243 <105318502+bodya4243@users.noreply.github.com>
2024-05-03 16:27:45 +0300

    1) add handling of other exception

--- src/main/java/com/example/onlinebookstore/dto/BookRequestDto.java
+++ src/main/java/com/example/onlinebookstore/dto/BookRequestDto.java
@@ -2,10 +2,10 @@ package com.example.onlinebookstore.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.ISBN;
 
 @Data
 public class BookRequestDto {
@@ -21,8 +21,7 @@ public class BookRequestDto {
     @Min(value = 0, message = "can't be less than 0")
     private BigDecimal price;
 
-    @NotNull
-    @Pattern(regexp = "\\d{13}", message = "ISBN must be a 13-digit number")
+    @ISBN
     private String isbn;
 
     private String description;
