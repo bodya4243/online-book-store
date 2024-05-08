3e3c3d5c569fe149463ea81d8e28de030887a88d
bodya4243 <105318502+bodya4243@users.noreply.github.com>
2024-05-08 17:26:17 +0300

    1) Add User Entity, controller, repository, mapper, service, DTO. 2) Add Registration Exception

---
+++ src/main/java/com/example/onlinebookstore/service/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.onlinebookstore.service;
+
+import com.example.onlinebookstore.dto.UserDto;
+import com.example.onlinebookstore.dto.UserRequestDto;
+import com.example.onlinebookstore.exception.RegistrationException;
+import com.example.onlinebookstore.mapper.UserMapper;
+import com.example.onlinebookstore.model.User;
+import com.example.onlinebookstore.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public UserDto registerUser(UserRequestDto userRequestDto) throws RegistrationException {
+        if (userRepository.findByEmail(userRequestDto.getEmail()).isPresent()) {
+            throw new RegistrationException("Can't find register user");
+        }
+        User user = userMapper.toModel(userRequestDto);
+
+        User savedUser = userRepository.save(user);
+        return userMapper.toDto(savedUser);
+    }
+}
